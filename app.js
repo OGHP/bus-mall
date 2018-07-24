de602b5415361a5498ddf2c3d4e17c18d91dbd56
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-07-23 19:06:16 -0700

    Start event listener

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ var images = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jp
 var imageData = [];
 var totalClicks = 0;
 
-//constructor
+
 function BusMallImages(images) {
     this.name = images.split('/')[1].split('.')[0];  //just the image NAME
     this.path = images;
     this.clicksPerImage = 0;
     this.timesDisplayed = 0;
 
-    //everything in the BusMallImages function goes into the array above
     imageData.push(this);
 };
 
-//push the image NAME, PATH, and CLICKS to the imageData array
+/********* creats objects from the constructor ********/
 function objectCreator() {
     for (var i = 0; i < images.length; i++) {
         new BusMallImages(images[i])
@@ -24,12 +23,11 @@ function objectCreator() {
 };
 objectCreator();
 
-
+/********* render three images ********/
 var image1 = document.getElementById('image-one');
 var image2 = document.getElementById('image-two');
 var image3 = document.getElementById('image-three');
 
-//render calls generateRandomNumber
 function renderThreeImages() {
     var randomNumber1 = Math.floor(Math.random() * (images.length - 1));
     var randomNumber2 = Math.floor(Math.random() * (images.length - 1));
@@ -44,24 +42,13 @@ function renderThreeImages() {
 
 renderThreeImages();
 
-// var imageTag1 = document.getElementById('image-one');
-
-// imageTag1.addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     var name = event.target.name.value;
-//     var path = event.target.minCustomer.value;
-//     var clicksPerImage = event.target.clicksPerImage.value;
-//     var timesDisplayed = event.target.timesDisplayed.value;
-
-// });
-
-document.getElementById("image-one").addEventListener("click", function (event) {
-    event.target.textContent = "click count: " + event.detail;
-});
+/********* event listener ********/
+// var imageOne = document.getElementById('image-one');
 
 
-//console.log(event.target.textContent = "click count: " + event.detail);
+// function eventListenerFunctionName() {
+//     increment tally for the image in the array
+// };
 
+// imageOne.addEventListener('click', eventListenerFunctionName());
 
-//listening for click on image
